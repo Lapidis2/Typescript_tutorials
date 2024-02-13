57ab169d754fe5e58d52f0fd7452e564712845ea
Lapidis2 <hitlapidis2@gmail.com>
2024-02-13 14:38:13 +0200

    typescript starts

---
+++ document.ts
@@ -0,0 +1,11 @@
+var area = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(area(6));
+var ninja = {
+    name: 'mula',
+    age: 30,
+    color: 'black',
+};
+ninja.name = 'mulo';
+ninja.age = 9;
